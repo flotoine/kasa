c25f7c7b143cc731fcea7a22022454e54a7f0341
flotoine <florian.antoine.m@gmail.com>
2024-05-02 12:59:17 +0200

    images in tiles (home)

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,9 +4,9 @@ export default function Card () {
     return (
         <div className="cards">
             {adList.map((ad) => (
-                <div className="card" key={ad.id}> 
-                    <p className="card__text">{ad.title}</p> 
-                    
+                <div className="card" key={ad.id} style={{backgroundImage: `linear-gradient(#ffffff00, #00000050), url(${ad.cover})` }}> 
+                   {/* <img className="card__image" src={ad.cover} alt={ad.title} />  Style dans div ???? */}
+                    <p className="card__text">{ad.title}</p>
                 </div>
             ))}
         </div>
