3541f52af4bb9543af2dda1329e8a1daf416cac0
flotoine <florian.antoine.m@gmail.com>
2024-05-14 12:03:46 +0200

    Animated about page

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -2,15 +2,28 @@ import unrollArrow from "../../assets/chevron-up-solid.svg"
 import "./Collapse.scss"
 
 export default function Collapse ({title, content}) {
+    function toggleDrawer () {
+        let drawer = document.getElementById(`collapseWrapper__textContainer${title}`)
+        drawer.className === "collapseWrapper__textContainer--closed collapseWrapper__textContainer" || 
+        drawer.className === "collapseWrapper__textContainer" ?
+            drawer.className = "collapseWrapper__textContainer--open collapseWrapper__textContainer" :
+            drawer.className = "collapseWrapper__textContainer--closed collapseWrapper__textContainer"
+
+        let arrow = document.getElementById(`collapseWrapper__top__arrow${title}`)
+        arrow.className === "collapseWrapper__top__arrow collapseWrapper__top__arrow--closed" ?
+            arrow.className = "collapseWrapper__top__arrow collapseWrapper__top__arrow--open" :
+            arrow.className = "collapseWrapper__top__arrow collapseWrapper__top__arrow--closed"
+    }
+
     return (
             <div className="collapseWrapper"> 
                 <div className="collapseWrapper__top">
                     <div className="collapseWrapper__top__title"> 
-                    {title}
+                        {title}
                     </div>
-                    <img className="collapseWrapper__top__arrow" src={unrollArrow} alt="bouton dérouler" />
-                </div >
-                <div className="collapseWrapper__textContainer">
+                    <img onClick={toggleDrawer} id={`collapseWrapper__top__arrow${title}`} className="collapseWrapper__top__arrow collapseWrapper__top__arrow--closed" src={unrollArrow} alt="bouton dérouler" />
+                </div>
+                <div id={`collapseWrapper__textContainer${title}`} className="collapseWrapper__textContainer">
                 <p>{content}</p>
                 </div>
             </div>
