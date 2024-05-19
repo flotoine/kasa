51910df754aa9a90530316c96b377f686b2131cb
flotoine <florian.antoine.m@gmail.com>
2024-05-19 13:53:47 +0200

    star rating display app

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,16 +1,23 @@
 import star from "../../assets/star-solid.svg"
+import "./StarRating.scss"
+
+const redStarString = `<img class="star__colored" src=${star} alt="étoile rouge">`
+const grayStarString = `<img class="star__gray" src=${star} alt="étoile grise">`
 
 export default function StarRating ({rating}) {
     let greyStars = 5 - rating;
     let output = [];
-    for (var i = rating; i >= 1; i--)
-        output.push('<img src={star} alt="étoile rouge">');
-/*
+    for (let i = rating; i >= 1; i--)
+        output.push(redStarString);
+
+    for (let j = greyStars; j >= 1; j--)
+        output.push(grayStarString)
+
     return (
-        <div className="ratingStars">  
-            {console.log(output.join(''))}  
+        <div className="star" dangerouslySetInnerHTML={{ __html: output.join('') }}>  
+            
         </div>
-    ) */
+    ) 
 }
 
 //travailler le join
